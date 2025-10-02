de4ebf642ac43553254c77194ad24efeaadf5d91
seonoleo <mscomy1@gmail.com>
2025-10-02 16:14:26 +0900

    6

--- main.c
+++ main.c
@@ -4,35 +4,28 @@
 int main(int argc, char *argv[])
 {
     
-    int a,b;
-    char op;
-    int result;
-    
-    printf("enter the calculation :");
-    scanf("%i %c %i" , &a, &op, &b);
-    
-    switch(op)
-    {
-    case '+':
-         result=a+b;
-         break;
-         
-    case '-':
-         result=a-b;
-         break;
-         
-    case '*':
-         result=a*b;
-         break;
-         
-    case '/':
-         result=a/b;
-         break;
-         
-        
-}
-    printf("= %i.\n",result);
+    int answer=59;
+    int num;
+    int trial=0;
      
+    
+    do
+    { 
+    printf("guess a number :");
+    scanf("%d", &num);
+    
+    if (answer<num)
+      printf("high!\n");
+    else if (answer > num)
+      printf("low!\n");
+       
+    trial++;
+        }
+    while(answer!=num);
+    printf("congratulation! trials:%i\n",trial);
+    
+   
+ 
   system("PAUSE");	
   return 0;
 }
